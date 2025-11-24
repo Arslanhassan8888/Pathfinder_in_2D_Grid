d18678fea7ab260d89fb5dbc16f995a0b5271781
Arslanhassan8888 <arslanarslan84@yahoo.com>
2025-11-24 20:22:09 +0000

    Added Map class with grid size, start position, and goal position
    Implemented the initial layout of the Map class.
    Included the rows and columns setup, created an empty grid placeholder,
    and defined the start point at the top-left corner and the goal point at the
    bottom-right corner.
    
    This is the foundation of the 2D world that the robot will navigate.

---
+++ main.py
@@ -0,0 +1,24 @@
+class Map:     
+    """
+    Simple 2D world for the robot.
+    We will use a grid with:
+      N = normal terrain
+      O = obstacle
+      W = water
+      H = hill
+    """
+    def __init__(self, rows, cols): # self refere to the object being created, in this case the l object.
+        # number of rows and columns
+        self.rows = rows
+        self.cols = cols
+
+        # this will the grid (list of lists)
+        self.grid = []
+
+        # set start and goal positions and start at top-left
+        # This is a tuple with row and column index
+        # tuple is immutable so cannot be changed and keep the same order of values
+        self.start = (0, 0)
+        # goal at bottom-right
+        # In this case have 10 rows and 10 columns so it will be 9,9, as indexing starts from 0 not 1
+        self.goal = (rows - 1, cols - 1) 
